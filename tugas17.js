7ba33864e2e2f0fc4b9caf7061040b52088ac676
syaifulfahmi <52196122+syaifulfahmi@users.noreply.github.com>
2020-09-21 11:57:13 +0700

    Add files via upload

---
+++ tugas17.js
@@ -0,0 +1,19 @@
+var kalimat1 = "Saya beLajar bahaSa peMrograman deNgan khUsuk."
+
+var kalimat2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual."
+
+var kalimat3 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+// var ascii = (kalimat3.substr(0,6))
+
+// function charCodeAt(nilai) {
+// console.log(nilai.charCodeAt())
+// }
+
+console.log (kalimat1.toLowerCase())
+console.log (kalimat1.toUpperCase())
+console.log (kalimat2.toLowerCase())
+console.log (kalimat2.toUpperCase())
+// charCodeAt (ascii)
+
+console.log(kalimat3.charCodeAt(0),"-",kalimat3.charCodeAt(1),"-",kalimat3.charCodeAt(2),"-",kalimat3.charCodeAt(3),"-",kalimat3.charCodeAt(4),"-",kalimat3.charCodeAt(5),"-",kalimat3.charCodeAt(6))
